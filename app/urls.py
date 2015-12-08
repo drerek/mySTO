f6122c27ed60d8da196b3de0fb7a4922daded1c2
mVad <vadimka12@ukr.net>
2015-12-08 20:28:15 +0200

    myUpdate5

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 
-from app.views import home, tabl, about
+from app.views import home, tabl, about, tableforeach
 
 urlpatterns = [
-    url(r'^home/', home),
     url(r'^table/all/$', tabl),
-    url(r'^table/get/(?<cars_id>)\d+$', tableforeach),
+    url(r'^table/get/(?P<cars_id>)\d+', tableforeach),
     url(r'^about/', about),
+    url(r'^', home),
             ]
