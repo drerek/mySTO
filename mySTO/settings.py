4a52956752d129238a4d7bd7e9ca0f3d2540c211
drerek <adrerek@gmail.com>
2015-12-14 19:39:06 +0200

    correct forma

--- mySTO/settings.py
+++ mySTO/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import pytz as pytz
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -109,7 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'uk-UA'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = pytz.timezone('Europe/Kiev')
+
+#TIME_ZONE = 'UTC+2:00'
 
 USE_I18N = True
 
