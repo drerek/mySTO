19c98cf1ec0ade26b52cbb1996899e70eb2dad11
drerek <adrerek@gmail.com>
2015-12-14 19:44:58 +0200

    Update models.py

--- forma/models.py
+++ forma/models.py
@@ -17,4 +17,4 @@ class Forma(models.Model):
     is_checked = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.date
+        return self.date.strftime("%Y-%m-%d %H:%M+02:00")
