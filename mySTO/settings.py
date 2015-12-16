a8e219e069853ed1ce56cb8474a75980b718aaa6
mVad <vadimka12@ukr.net>
2015-12-16 15:42:22 +0200

    uppdate

--- mySTO/settings.py
+++ mySTO/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    ('static', 'E:\Django\Projects\mySTOgit\static'),
+    ('static', 'D:\PycharmProjects\mySTO(update5)\static'),
 
 )
 
