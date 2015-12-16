9891c7519da755184540100e9397695eb53132d5
ASTamichan <artichsa@yandex.ua>
2015-12-16 03:10:53 +0200

    Login Front

--- mySTO/settings.py
+++ mySTO/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -127,7 +126,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    ('static', 'D:\PycharmProjects\mySTO(update5)\static'),
+    ('static', 'E:\Django\Projects\mySTOgit\static'),
 
 )
 
