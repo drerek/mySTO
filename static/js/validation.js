1d4ebdbecd7f93f68a5ba7672e583602562b7c98
ASTamichan <artichsa@yandex.ua>
2015-12-31 14:26:35 +0200

    Validation forma

--- static/js/validation.js
+++ static/js/validation.js
@@ -63,11 +63,11 @@
 				 if (!is_d && digits.indexOf(pas1[i]) != -1){
 					 is_d=true;
 				 }
-				}
+			    }
 			 if(is_s==false || is_d==false){
 				 document.getElementById('password1').innerHTML = 'Пароль повинен містити букви і цифри!';
 				 k=1;
-			 } 
+			    } 
 		}
 	}
 	if(pas2==null || pas2=="") {
@@ -79,6 +79,51 @@
 			document.getElementById('password2').innerHTML = 'Паролі не співпадають!';
 			k=1;
 		}
+	}
+	if(k==1){
+		return false;	
+	}
+}
+function validateForm(){
+	var ln=document.forms["forma"]["last_name"].value;
+	var fn=document.forms["forma"]["first_name"].value;
+	var date=document.forms["forma"]["date"].value;
+	var time=document.forms["forma"]["time"].value;
+	var cn = document.getElementById("id_car_name").options.selectedIndex;
+	var txt = document.getElementById("id_car_name").options[cn].text;
+	var k=0;
+	var symb = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+	var is_s=false;
+	document.getElementById('first_name').innerHTML = '';
+	document.getElementById('last_name').innerHTML = '';
+	document.getElementById('datetime').innerHTML = '';
+	document.getElementById('car_name').innerHTML = '';
+	if(ln==null || ln=="") {
+		document.getElementById('last_name').innerHTML = 'Це поле обов\'язкове для заповнення!';
+		k=1;
+	}
+	if(fn==null || fn=="") {
+		document.getElementById('first_name').innerHTML = 'Це поле обов\'язкове для заповнення!';
+		k=1;
+	}
+	if(date==null || date==""||time==null || time==""){
+		document.getElementById('datetime').innerHTML = 'Ці поля обов\'язкові для заповнення!';
+		k=1;
+	}
+	else{
+		for (var i = 0; i < time.length; i++) {
+				 if (!is_s && symb.indexOf(time[i]) != -1){
+					is_s=true; 
+				 }
+	    }
+		if(time[2]!=":" ||is_s==true){
+			document.getElementById('datetime').innerHTML = 'Непрвильний формат часу!';
+			k=1;
+		}
+	}
+	if(txt=="---------"){
+		document.getElementById('car_name').innerHTML = 'Оберіть авто!';
+		k=1;
 	}
 	if(k==1){
 		return false;	
