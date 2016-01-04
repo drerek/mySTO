da675edcd0cf44e4e69ea1aab84dcc2ca8765374
ASTamichan <artichsa@yandex.ua>
2016-01-04 22:41:24 +0200

    Active head table

--- static/js/active.js
+++ static/js/active.js
@@ -3,5 +3,10 @@
     if (window.location.href == this.href) {
       $(this).closest('li').addClass('active');
     }
+	else{
+		if(window.location.href == "http://127.0.0.1:8000/table/get/1" || window.location.href == "http://127.0.0.1:8000/table/get/2"|| window.location.href == "http://127.0.0.1:8000/table/get/3"|| window.location.href == "http://127.0.0.1:8000/table/get/4") {
+			$('#price').addClass('active');
+		}
+	}
   });
 });
